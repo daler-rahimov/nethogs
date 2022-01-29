c4ef5d2d25f53a4c3818fc473b97bf481793d356
Daler Rahimov <daler.edu@gmail.com>
2022-01-29 01:44:53 -0500

    FIX: seg fault due to string overflow

--- src/inode2prog.cpp
+++ src/inode2prog.cpp
@@ -120,10 +120,26 @@ std::string getcmdline(pid_t pid) {
   const int maxfilenamelen = 14 + MAX_PID_LENGTH + 1;
   char filename[maxfilenamelen];
 
+  std::cerr << "Calling getcmdline, pid: " << pid << std::endl;
   std::snprintf(filename, maxfilenamelen, "/proc/%d/cmdline", pid);
-
+  std::string cmdline;
   bool replace_null = false;
-  std::string cmdline = read_file(filename);
+  try {
+    cmdline = read_file(filename);
+  } catch (const char *e) {
+    std::fprintf(stderr, "An exception occurred. Exception %s \n", e);
+    cmdline = "";
+  } catch (...) {
+    std::fprintf(stderr, "An exception occurred while reading cmdline.\n");
+    cmdline = "";
+  }
+
+  if (cmdline.empty() || cmdline[cmdline.length() - 1] != '\0') {
+    // invalid content of cmdline file. Add null char to allow further
+    // processing.
+    cmdline.append(1, '\0');
+    return cmdline;
+  }
 
   // join parameters, keep prgname separate, don't overwrite trailing null
   for (size_t idx = 0; idx < (cmdline.length() - 1); idx++) {
@@ -134,15 +150,9 @@ std::string getcmdline(pid_t pid) {
       replace_null = true;
     }
   }
-
-  if (cmdline.length() == 0 || (cmdline[cmdline.length() - 1] != 0x00)) {
-    // invalid content of cmdline file. Add null char to allow further
-    // processing.
-    cmdline.append("\0");
-  }
-
   return cmdline;
 }
+
 
 void setnode(unsigned long inode, pid_t pid) {
   prg_node *current_value = inodeproc[inode];
